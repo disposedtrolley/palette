14c20632faa5847cb6d157f0717f770b12c6b3c1
James Liu <james@jamesliu.io>
2021-03-26 22:21:06 +1100

    feat: read image

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
+#define STB_IMAGE_IMPLEMENTATION
+
 #include <stdio.h>
+#include <stb_image.h>
 
 int main(void) {
-    printf("Hello");
+    int x, y, n;
+    unsigned char *data = stbi_load("/Users/james/Desktop/_DSF0914.jpg", &x, &y, &n, 0);
+    if (data == NULL) {
+        return 1;
+    }
+
+    printf("x: %d y: %d n: %d\n", x, y, n);
+
+    stbi_image_free(data);
+
     return 0;
 }
