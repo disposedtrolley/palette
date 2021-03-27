816cf119138b415f45b895e9bd4645fddb1c5748
James Liu <james@jamesliu.io>
2021-03-27 12:49:23 +1100

    feat: iterate over each pixel

--- src/main.c
+++ src/main.c
@@ -1,18 +1,31 @@
-#define STB_IMAGE_IMPLEMENTATION
-
 #include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 int main(void) {
     int x, y, n;
-    unsigned char *data = stbi_load("/Users/james/Desktop/_DSF0914.jpg", &x, &y, &n, 0);
-    if (data == NULL) {
+    unsigned char *img = stbi_load("/Users/james/Desktop/_DSF0914.jpg", &x, &y, &n, 0);
+    if (img == NULL) {
         return 1;
+    }
+
+//    size_t img_size = x * y * n;
+    size_t img_size = n * 10;  // first 10 pixels
+
+    for (unsigned char *p = img; p != img + img_size; p += n) {
+        uint8_t r = *p;
+        uint8_t g = *(p + 1);
+        uint8_t b = *(p + 2);
+
+        printf("R: %d G: %d B: %d\n", r, g, b);
     }
 
     printf("x: %d y: %d n: %d\n", x, y, n);
 
-    stbi_image_free(data);
+    stbi_write_jpg("/Users/james/Desktop/out.jpg", x, y, n, img, 100);
 
+    stbi_image_free(img);
     return 0;
 }
